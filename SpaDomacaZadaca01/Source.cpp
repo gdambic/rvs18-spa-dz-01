e4a17da799105bfb747f562921ed4ac7f49db9f9
jpavic <jpavic07@gmail.com>
2018-03-25 21:36:53 +0200

    implementacija zadace

--- SpaDomacaZadaca01/Source.cpp
+++ SpaDomacaZadaca01/Source.cpp
@@ -1,10 +1,18 @@
 #include <SFML/Graphics.hpp>
+#include "Cvijet.h"
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(400, 400), "Hello, SFML world!");
+	sf::RenderWindow window(sf::VideoMode(500, 500), "Hello, SFML world!");
 	window.setFramerateLimit(60);
-	//Cvijet cvijet(&window);
+	Cvijet cvijet(&window);
+
+	//Sunce
+	cvijet.set_radius_sunce(30);
+	cvijet.set_color_sunce(sf::Color::Yellow);
+	cvijet.set_position_sunce(10, 10);
+	cvijet.set_color_SunceRub(255,193,7);
+
 
 	while (window.isOpen())
 	{
@@ -15,8 +23,8 @@ int main()
 				window.close();
 		}
 
-		window.clear();
-		//cvijet.draw();
+		window.clear(sf::Color(190, 225, 255));
+		cvijet.draw();
 		window.display();
 	}
 
