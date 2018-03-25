e78a7b8fee32d8c5bf30f9059378ae0454f7ab62
majankov <jankovic.mach@gmail.com>
2018-03-25 23:16:54 +0200

    commit

---
+++ SpaDomacaZadaca01/Cvijet.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+
+class Cvijet
+{
+public:
+	Cvijet(sf::RenderWindow* window);
+	void draw();
+	
+
+private:
+	sf::Clock frameClock;
+	sf::RenderWindow *mwindow;
+};
+
